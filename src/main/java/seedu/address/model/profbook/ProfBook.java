3d337fbffbfbe672334979c75752c46842415b6b
mingyuanc <cmingyuan123@gmail.com>
2023-09-29 06:40:34 +0800

    Create ProfBook.java

--- src/main/java/seedu/address/model/profbook/ProfBook.java
+++ src/main/java/seedu/address/model/profbook/ProfBook.java
@@ -1,7 +1,20 @@
 package seedu.address.model.profbook;
+import java.util.Map;
+
+import seedu.address.model.taskmanager.TaskList;
 
 /**
  * Encapsulates logic for the whole application data
  */
-public class ProfBook {
+public class ProfBook extends ProfBookDir<Group> {
+
+    /**
+     * Constructs a new prof book instance
+     *
+     * @param taskList - The Task list associated with this object
+     * @param children - The Groups under the root
+     */
+    public ProfBook(TaskList taskList, Map<Id, Group> children) {
+        super(taskList, children);
+    }
 }
