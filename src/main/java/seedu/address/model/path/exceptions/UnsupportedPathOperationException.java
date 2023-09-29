a6ed62f0385a3f27b3a38e715ad81bfc71461ec8
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-09-29 14:51:33 +0800

    Add unsupportedPathOperationException

---
+++ src/main/java/seedu/address/model/path/exceptions/UnsupportedPathOperationException.java
@@ -0,0 +1,17 @@
+package seedu.address.model.path.exceptions;
+
+/**
+ * The {@code UnsupportedPathOperationException} class represents an 
+ * exception that is thrown when an operation is not supported or is 
+ * invalid based on the state of a path directory.
+ */
+public class UnsupportedPathOperationException extends Exception {
+    /**
+     * Constructs an {@code UnsupportedPathOperationException} with the specified error message.
+     *
+     * @param message The error message describing why the operation is not supported or invalid.
+     */
+    public UnsupportedPathOperationException(String message) {
+        super(message);
+    }
+}
