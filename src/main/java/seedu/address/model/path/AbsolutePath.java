8bff8b0251d4422518a545032a334cb92ccf4b9e
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-09-29 14:29:45 +0800

    Add javadoc comments

--- src/main/java/seedu/address/model/path/AbsolutePath.java
+++ src/main/java/seedu/address/model/path/AbsolutePath.java
@@ -55,17 +55,32 @@ public class AbsolutePath extends Path {
 
         return new AbsolutePath(fullPathElements);
     }
-
+    
+    /**
+     * Checks whether the path represents a group directory.
+     *
+     * @return {@code true} if the path is a group directory; {@code false} otherwise.
+     */
     public boolean isGroupDirectory() {
         PathElement lastElement = this.pathElements.get(this.pathElements.size() - 1);
         return lastElement.getType() == PathElementType.GROUPID;
     }
 
+    /**
+     * Checks whether the path represents a student directory.
+     *
+     * @return {@code true} if the path is a student directory; {@code false} otherwise.
+     */
     public boolean isStudentDirectory() {
         PathElement lastElement = this.pathElements.get(this.pathElements.size() - 1);
         return lastElement.getType() == PathElementType.STUDENTID;
     }
 
+    /**
+     * Checks whether the path represents a root directory.
+     *
+     * @return {@code true} if the path is a root directory; {@code false} otherwise.
+     */
     public boolean isRootDirectory() {
         PathElement lastElement = this.pathElements.get(this.pathElements.size() - 1);
         return lastElement.getType() == PathElementType.ROOT;
