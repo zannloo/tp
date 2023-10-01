755aa27b1d3728cc7889788ba24d0effc5ccca48
Loo Mei Lin Zann <>
2023-10-01 23:22:50 +0800

    Create initial command class skeletons
    Create a new package, newcommands, for the new command classes (
    CreateDeadlineForStudentCommand, CreateStudentCommand and
    CreateToDoForStudentCommand).
    
    Comment out certain unused codes to fix CheckStyle errors.

---
+++ src/main/java/seedu/address/logic/newcommands/Command.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.newcommands;
+
+import java.util.List;
+
+import seedu.address.logic.newcommands.exceptions.CommandException;
+import seedu.address.model.path.AbsolutePath;
+import seedu.address.model.path.element.PathElement;
+import seedu.address.model.profbook.Root;
+
+/**
+ * Represents a command with hidden internal logic and the ability to be executed.
+ */
+public abstract class Command {
+
+    /**
+     * Executes the command and returns the result message.
+     *
+     * @param currPath {@code currPath} .
+     * @param root {@code root} which the command should operate on.
+     * @return feedback message of the operation result for display
+     * @throws CommandException If an error occurs during command execution.
+     */
+    public abstract CommandResult execute(AbsolutePath currPath, Root root) throws CommandException;
+}
---
+++ src/main/java/seedu/address/logic/newcommands/CreateDeadlineForStudentCommand.java
@@ -0,0 +1,55 @@
+package seedu.address.logic.newcommands;
+
+//import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.List;
+
+//import seedu.address.commons.util.ToStringBuilder;
+//import seedu.address.logic.Messages;
+import seedu.address.logic.newcommands.exceptions.CommandException;
+import seedu.address.model.path.AbsolutePath;
+import seedu.address.model.path.RelativePath;
+//import seedu.address.model.path.element.PathElement;
+import seedu.address.model.profbook.Root;
+import seedu.address.model.taskmanager.Deadline;
+//import seedu.address.model.taskmanager.Task;
+
+/**
+ * Adds a person to the address book.
+ */
+public class CreateDeadlineForStudentCommand extends Command {
+
+    public static final String COMMAND_WORD = "deadline";
+
+    public static final String MESSAGE_SUCCESS = "New Deadline task added: %1$s";
+    public static final String MESSAGE_DUPLICATE_TODO_TASK =
+            "This Deadline task has already been allocated to this student in ProfBook";
+
+    private final RelativePath path;
+    private final Deadline deadline;
+
+    /**
+     * Creates an CreateDeadlineForStudentCommand to add the Deadline Task for a specified {@code Student}
+     */
+    public CreateDeadlineForStudentCommand(RelativePath path, Deadline deadline) {
+        requireAllNonNull(path, deadline);
+        this.path = path;
+        this.deadline = deadline;
+    }
+
+    @Override
+    public CommandResult execute(AbsolutePath currPath, Root root) throws CommandException {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
