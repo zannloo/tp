315cdd697b75fbeb88143b4163a90e1f16bc53ac
Loo Mei Lin Zann <>
2023-10-03 12:21:29 +0800

    Remove unused import statements

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -1,11 +1,7 @@
 package seedu.address.logic.commands;
 
-//import java.util.List;
-
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-//import seedu.address.model.path.element.PathElement;
-//import seedu.address.model.profbook.Root;
 
 /**
  * Represents a command with hidden internal logic and the ability to be executed.
