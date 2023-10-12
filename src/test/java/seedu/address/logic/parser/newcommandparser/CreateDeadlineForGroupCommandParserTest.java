663c0584507995f0539cc9b521a808742ca2bc48
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-10-12 08:42:29 +0800

    Add test for create deadline and todo

---
+++ src/test/java/seedu/address/logic/parser/newcommandparser/CreateDeadlineForGroupCommandParserTest.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.parser.newcommandparser;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.newcommands.Command;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class CreateDeadlineForGroupCommandParserTest {
+    private CreateDeadlineForGroupCommandParser parser = new CreateDeadlineForGroupCommandParser();
+    private static final Logger logger = LogsCenter.getLogger(CreateDeadlineForGroupCommandParser.class);
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+        String argument = "~/grp-001/ -d assignment 2 -dt 2023-09-22 11:30";
+
+        try {
+            Command command = parser.parse(argument);
+            logger.info(command.toString());
+        } catch (ParseException pe) {
+            logger.warning(pe.getMessage());
+            // throw new IllegalArgumentException("Invalid userInput.", pe);
+        }
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/newcommandparser/CreateTodoForGroupCommandParserTest.java
@@ -0,0 +1,27 @@
+package seedu.address.logic.parser.newcommandparser;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.newcommands.Command;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class CreateTodoForGroupCommandParserTest {
+    private CreateTodoForGroupCommandParser parser = new CreateTodoForGroupCommandParser();
+    private static final Logger logger = LogsCenter.getLogger(CreateTodoForGroupCommandParser.class);
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+        String argument = "~/grp-001/ -d assignment 1";
+
+        try {
+            Command command = parser.parse(argument);
+            logger.info(command.toString());
+        } catch (ParseException pe) {
+            logger.warning(pe.getMessage());
+            // throw new IllegalArgumentException("Invalid userInput.", pe);
+        }
+    }
+}
