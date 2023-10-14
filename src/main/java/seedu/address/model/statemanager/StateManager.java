b3d307a64b8e6369dd915329eeb29faa30dd5d20
Ming Yuan <cmingyuan123@gmail.com>
2023-10-15 05:34:59 +0800

    Refactor Old operations to Task and Child Operation

--- src/main/java/seedu/address/model/statemanager/StateManager.java
+++ src/main/java/seedu/address/model/statemanager/StateManager.java
@@ -2,93 +2,85 @@ package seedu.address.model.statemanager;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.logging.Logger;
 
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.id.GroupId;
 import seedu.address.model.id.StudentId;
-import seedu.address.model.id.exceptions.InvalidIdException;
 import seedu.address.model.path.AbsolutePath;
 import seedu.address.model.path.exceptions.UnsupportedPathOperationException;
 import seedu.address.model.profbook.Group;
 import seedu.address.model.profbook.Root;
 import seedu.address.model.profbook.Student;
-import seedu.address.model.profbook.TaskListManager;
 import seedu.address.model.profbook.exceptions.NoSuchChildException;
-import seedu.address.model.taskmanager.Task;
-import seedu.address.model.taskmanager.exceptions.NoSuchTaskException;
 
 /**
  * Encapsulates logic manipulating data in ProfBook
  */
-public class StateManager implements ITaskOperations {
+public class StateManager {
 
-    private final Logger logger = LogsCenter.getLogger(JsonUtil.class);
-
-    private final TaskListManager baseDir;
 
     /**
-     * Constructs a new stateManager instance
-     *
-     * @param baseDir - The current base directory of the state Manager
-     */
-    public StateManager(TaskListManager baseDir) {
-        this.baseDir = baseDir;
-    }
-
-    /**
-     * Creates a rootOperation class performs operation on the root
-     *
-     * @param root - The current root instance
-     * @return A rootOperation instance specific to current root
-     */
-    public static RootOperation rootOperation(Root root) {
-        requireAllNonNull(root);
-        return new RootOperation(root);
-    }
-
-    /**
-     * Creates a GroupOperation class performs operation on the specified group
+     * Creates a ChildOperation class performs operation on the specified root
      *
      * @param root        - The current root instance
-     * @param pathToGroup - The path to the specified group
      * @return A groupOperation instance specific to current Group
      * @throws UnsupportedPathOperationException If the operation is not supported
      *                                           based on the directory's state.
-     * @throws InvalidIdException                If the retrieved ID is invalid.
      * @throws NoSuchChildException              If the path does not lead to a
-     *                                           valid group
+     *                                           valid childManager
      */
-    public static GroupOperation groupOperation(Root root, AbsolutePath pathToGroup)
+    public static ChildOperation<Group> rootChildOperation(Root root)
             throws UnsupportedPathOperationException, NoSuchChildException {
-        requireAllNonNull(root, pathToGroup);
-        Group currDir = StateManager.getGroupFromPath(root, pathToGroup);
-        return new GroupOperation(currDir);
+        return new ChildOperation<>(root);
     }
 
     /**
-     * Creates a StudentOperation class performs operation on the specified Student
+     * Creates a ChildOperation class performs operation on the specified group
      *
-     * @param root          - The current root instance
-     * @param pathToStudent - The path to the specified student
-     * @return A StudentOperation instance specific to current Stident
+     * @param root        - The current root instance
+     * @param path - The path to the childManager
+     * @return A groupOperation instance specific to current Group
      * @throws UnsupportedPathOperationException If the operation is not supported
      *                                           based on the directory's state.
-     * @throws InvalidIdException                If the retrieved ID is invalid.
      * @throws NoSuchChildException              If the path does not lead to a
-     *                                           valid student
+     *                                           valid childManager
      */
-    public static StudentOperation studentOperation(Root root, AbsolutePath pathToStudent)
+    public static ChildOperation<Student> groupChildOperation(Root root, AbsolutePath path)
             throws UnsupportedPathOperationException, NoSuchChildException {
-        requireAllNonNull(root, pathToStudent);
-        Student currDir = StateManager.getStudentFromPath(root, pathToStudent);
-        return new StudentOperation(currDir);
+        requireAllNonNull(root, path);
+        if (!path.isGroupDirectory()) {
+            throw new UnsupportedPathOperationException("Not a group directory");
+        }
+        return new ChildOperation<>(StateManager.getGroupFromPath(root, path));
     }
 
-    public static Group getGroupFromPath(Root root, AbsolutePath p)
+    /**
+     * Creates a TaskOperation class performs task operation on the specified directory
+     *
+     * @param root          - The current root instance
+     * @param path - The path to the specified student
+     * @return A TaskOperation instance specific to current path
+     * @throws UnsupportedPathOperationException If the operation is not supported
+     *                                           based on the directory's state.
+     * @throws NoSuchChildException              If the path does not lead to a
+     *                                           valid directory
+     */
+    public static TaskOperation taskOperation(Root root, AbsolutePath path)
+            throws UnsupportedPathOperationException, NoSuchChildException {
+        requireAllNonNull(root, path);
+
+        if (path.isRootDirectory()) {
+            return new TaskOperation(root);
+        } else if (path.isGroupDirectory()) {
+            return new TaskOperation(getGroupFromPath(root, path));
+        } else if (path.isStudentDirectory()) {
+            return new TaskOperation(getStudentFromPath(root, path));
+        } else {
+            throw new RuntimeException("Not supposed to reach here");
+        }
+    }
+
+    static Group getGroupFromPath(Root root, AbsolutePath p)
             throws UnsupportedPathOperationException, NoSuchChildException {
         Optional<GroupId> groupIdOptional = p.getGroupId();
         if (groupIdOptional.isPresent()) {
@@ -97,7 +89,7 @@ public class StateManager implements ITaskOperations {
         throw new UnsupportedPathOperationException("No such group at " + p);
     }
 
-    public static Student getStudentFromPath(Root root, AbsolutePath p)
+    static Student getStudentFromPath(Root root, AbsolutePath p)
             throws UnsupportedPathOperationException, NoSuchChildException {
         Optional<GroupId> groupIdOptional = p.getGroupId();
         Optional<StudentId> studentIdOptional = p.getStudentId();
@@ -107,97 +99,4 @@ public class StateManager implements ITaskOperations {
         throw new UnsupportedPathOperationException("No such student at " + p);
     }
 
-    void stateLogger(String log) {
-        this.logger.info(log);
-    }
-
-    void stateErrorLogger(String errMsg) {
-        this.logger.severe(errMsg);
-    }
-
-    /**
-     * Adds a new tasks to the task list
-     *
-     * @param task
-     */
-    @Override
-    public void addTask(Task task) {
-        this.baseDir.addTask(task);
-        this.stateLogger("Adding" + task.toString());
-    }
-
-    /**
-     * Deletes the task at the specified index
-     *
-     * @param index - The index of the targeted class
-     * @return The deleted class
-     * @throws NoSuchTaskException if no task can be found by the index
-     */
-    @Override
-    public Task deleteTask(int index) throws NoSuchTaskException {
-        this.stateLogger("deleting " + index);
-        return this.baseDir.deleteTask(index);
-    }
-
-    /**
-     * Marks the task at the specified index as completed
-     *
-     * @param index - The index of the targeted class
-     * @return The marked task
-     * @throws NoSuchTaskException if no task can be found by the index
-     */
-    @Override
-    public Task markTask(int index) throws NoSuchTaskException {
-        this.stateLogger("marking " + index);
-        return this.baseDir.markTask(index);
-    }
-
-    /**
-     * Marks the task at the specified index as not completed
-     *
-     * @param index - The index of the targeted class
-     * @return The un-marked task
-     * @throws NoSuchTaskException if no task can be found by the index
-     */
-    @Override
-    public Task unmarkTask(int index) throws NoSuchTaskException {
-        this.stateLogger("un marking " + index);
-        return this.baseDir.unmarkTask(index);
-    }
-
-    /**
-     * Finds all matching task, compares by the task's description
-     *
-     * @param query - The String to match
-     * @return A list of all matching Tasks
-     */
-    @Override
-    public List<Task> findTask(String query) throws NoSuchTaskException {
-        this.stateLogger("finding " + query);
-        return this.baseDir.findTask(query);
-    }
-
-    /**
-     * Returns the task at the specified index
-     *
-     * @param index - The index of the targeted class
-     * @return The specified task
-     * @throws NoSuchTaskException if no task can be found by the index
-     */
-    @Override
-    public Task getTask(int index) throws NoSuchTaskException {
-        this.stateLogger("getting " + index);
-        return this.baseDir.getTask(index);
-    }
-
-    /**
-     * Returns all current task
-     *
-     * @return A list of all Tasks
-     */
-    @Override
-    public List<Task> getAllTasks() throws NoSuchTaskException {
-        this.stateLogger("getting all ");
-        return this.baseDir.getAllTask();
-    }
 }
