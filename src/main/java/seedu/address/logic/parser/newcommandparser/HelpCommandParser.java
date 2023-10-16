2070294d90aa69c778c612a4de213e6b1636f85f
NgChunMan <chunman03@gmail.com>
2023-10-16 09:15:43 +0800

    Add HelpCommandTest and update HelpCommandParser

--- src/main/java/seedu/address/logic/parser/newcommandparser/HelpCommandParser.java
+++ src/main/java/seedu/address/logic/parser/newcommandparser/HelpCommandParser.java
@@ -1,23 +1,13 @@
 package seedu.address.logic.parser.newcommandparser;
 
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
 import seedu.address.logic.newcommands.HelpCommand;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-/**
- * Parses user input to create a `HelpCommand`. This parser handles commands to display help messages.
- */
-public class HelpCommandParser implements Parser<HelpCommand> {
-    /**
-     * Parses the given command arguments and creates a `HelpCommand` object.
-     *
-     * @param args The command arguments to be parsed.
-     * @return A `HelpCommand` object.
-     * @throws ParseException If the command arguments are invalid or if parsing fails.
-     */
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+public class HelpCommandParser  implements Parser<HelpCommand> {
     public HelpCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args);
---
+++ src/test/java/seedu/address/logic/newcommands/HelpCommandTest.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.newcommands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.newcommands.exceptions.CommandException;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.id.Id;
+import seedu.address.model.path.AbsolutePath;
+import seedu.address.model.path.exceptions.InvalidPathException;
+import seedu.address.model.profbook.Group;
+import seedu.address.model.profbook.Root;
+import seedu.address.model.statemanager.State;
+import seedu.address.model.taskmanager.TaskList;
+
+public class HelpCommandTest {
+    @Test
+    public void execute_validCommand_success() throws CommandException, InvalidPathException {
+        TaskList taskList = new TaskList(new ArrayList<>());
+        Map<Id, Group> children = new HashMap<>();
+        Root root = new Root(taskList, children);
+        AbsolutePath currPath = new AbsolutePath("~/");
+        State state = new State(currPath, root, new UserPrefs());
+
+        HelpCommand helpCommand = new HelpCommand();
+        CommandResult commandResult = helpCommand.execute(state);
+
+        CommandResult expexctedCommandResult =
+                new CommandResult(HelpCommand.SHOWING_HELP_MESSAGE, true, false);
+        assertEquals(commandResult, expexctedCommandResult);
+    }
+}
