0fac334199b9162f9cca4ffaf143928f63059073
NgChunMan <chunman03@gmail.com>
2023-10-16 09:55:52 +0800

    Update checkstyle

--- src/main/java/seedu/address/logic/parser/newcommandparser/HelpCommandParser.java
+++ src/main/java/seedu/address/logic/parser/newcommandparser/HelpCommandParser.java
@@ -1,13 +1,24 @@
 package seedu.address.logic.parser.newcommandparser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.logic.newcommands.HelpCommand;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
-public class HelpCommandParser  implements Parser<HelpCommand> {
+/**
+ * The HelpCommandParser class is responsible for parsing user input and creating a HelpCommand
+ * based on the input. It implements the Parser interface for parsing commands.
+ */
+public class HelpCommandParser implements Parser<HelpCommand> {
+    /**
+     * Parses the user input to create a HelpCommand.
+     *
+     * @param args The user input string.
+     * @return A HelpCommand based on the input.
+     * @throws ParseException If the user input is invalid or in the wrong format.
+     */
     public HelpCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args);
