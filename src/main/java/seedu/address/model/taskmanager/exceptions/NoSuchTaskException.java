d25f055bd4b8ad900411c9575e64ee717a2620de
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-10-17 13:57:51 +0800

    Change to run time exception

--- src/main/java/seedu/address/model/taskmanager/exceptions/NoSuchTaskException.java
+++ src/main/java/seedu/address/model/taskmanager/exceptions/NoSuchTaskException.java
@@ -3,7 +3,7 @@ package seedu.address.model.taskmanager.exceptions;
 /**
  * Signals when the index does not point to a specific task in the tasklist
  */
-public class NoSuchTaskException extends Exception {
+public class NoSuchTaskException extends RuntimeException {
     public NoSuchTaskException(String e) {
         super(e);
     }
