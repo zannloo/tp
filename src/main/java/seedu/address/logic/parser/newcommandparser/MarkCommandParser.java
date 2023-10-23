cdd9e66f333e9e2feb2122b20a57f5dd87c8ddab
NgChunMan <chunman03@gmail.com>
2023-10-23 14:12:26 +0800

    Add skeleton file for MarkCommandParser and UnmarkCommandParser

---
+++ src/main/java/seedu/address/logic/parser/newcommandparser/MarkCommandParser.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.parser.newcommandparser;
+
+import seedu.address.logic.newcommands.MarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.path.AbsolutePath;
+
+public class MarkCommandParser implements Parser<MarkCommandParser> {
+
+    public MarkCommand parse(String args, AbsolutePath currPath) throws ParseException {
+        return null;
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/newcommandparser/UnmarkCommandParser.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.parser.newcommandparser;
+
+import seedu.address.logic.newcommands.UnmarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.path.AbsolutePath;
+
+public class UnmarkCommandParser implements Parser<UnmarkCommandParser> {
+
+    public UnmarkCommand parse(String args, AbsolutePath currPath) throws ParseException {
+        return null;
+    }
+}
