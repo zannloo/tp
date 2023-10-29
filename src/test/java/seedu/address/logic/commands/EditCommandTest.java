f4022d034de66d32716e863c477628c29136ecb4
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-10-30 02:35:15 +0800

    Refactor test cases to use newly added test utils

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -2,26 +2,40 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.EditCommand.ERROR_MESSAGE_NO_SUCH_GROUP;
+import static seedu.address.logic.commands.EditCommand.MESSAGE_INCORRECT_DIRECTORY_ERROR;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalGroups.GROUP_ONE;
+import static seedu.address.testutil.TypicalGroups.GROUP_TWO;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.field.EditGroupDescriptor;
 import seedu.address.model.field.EditStudentDescriptor;
-import seedu.address.model.id.Id;
 import seedu.address.model.path.AbsolutePath;
+import seedu.address.model.path.RelativePath;
 import seedu.address.model.path.exceptions.InvalidPathException;
 import seedu.address.model.profbook.Group;
+import seedu.address.model.profbook.Name;
 import seedu.address.model.profbook.Root;
+import seedu.address.testutil.RootBuilder;
 
 public class EditCommandTest {
+    private Model model;
+    private Model expectedModel;
+    private AbsolutePath rootPath = CommandTestUtil.getValidRootAbsolutePath();
+
+    @BeforeEach
+    public void setup() {
+        Root root = new RootBuilder().withGroup(GROUP_ONE).build();
+        model = new ModelManager(rootPath, new Root(root), new UserPrefs());
+        expectedModel = new ModelManager(rootPath, new Root(root), new UserPrefs());
+    }
 
     @Test
     public void constructor_nullRelativePathAndEditGroupDescriptor_throwsNullPointerException() {
@@ -34,53 +48,63 @@ public class EditCommandTest {
     }
 
     @Test
-    public void equals_differentRelativePath_fail() throws InvalidPathException {
-        AbsolutePath path1 = new AbsolutePath("~/grp-001");
-        AbsolutePath path2 = new AbsolutePath("~/grp-002");
-        EditGroupDescriptor editGroupDescriptor = new EditGroupDescriptor();
-        EditCommand firstEditCommand = new EditCommand(path1, editGroupDescriptor);
-        EditCommand secondEditCommand = new EditCommand(path2, editGroupDescriptor);
-
-        assertNotEquals(firstEditCommand, secondEditCommand);
-    }
-
-    @Test
-    public void toString_validateOutputString_correctStringRepresentation() throws InvalidPathException {
-        AbsolutePath path = new AbsolutePath("~/grp-001");
-        EditGroupDescriptor editGroupDescriptor = new EditGroupDescriptor();
-        EditCommand firstEditCommand = new EditCommand(path, editGroupDescriptor);
-
-        String expected = "seedu.address.logic.commands.EditCommand{toEdit=seedu.address.model"
-                + ".field.EditGroupDescriptor{name=null, id=null}}";
-        assertEquals(expected, firstEditCommand.toString());
-    }
-
-    @Test
     public void execute_noSuchGroup_throwCommandException() throws InvalidPathException {
-        Map<Id, Group> children = new HashMap<>();
-        Root root = new Root(children);
-        AbsolutePath currPath = new AbsolutePath("~/");
-        AbsolutePath path = new AbsolutePath("~/grp-001");
-        Model model = new ModelManager(currPath, root, new UserPrefs());
+        Group groupNotInRoot = GROUP_TWO;
 
-        EditStudentDescriptor editStudentDescriptor = new EditStudentDescriptor();
-        EditCommand editCommand = new EditCommand(path, editStudentDescriptor);
+        RelativePath groupPath = new RelativePath(groupNotInRoot.getId().toString());
+        AbsolutePath targetAbsolutePath = rootPath.resolve(groupPath);
 
-        assertThrows(CommandException.class, EditCommand.MESSAGE_NO_SUCH_PATH, () -> editCommand.execute(model));
+        EditGroupDescriptor editStudentDescriptor = new EditGroupDescriptor();
+        EditCommand command = new EditCommand(targetAbsolutePath, editStudentDescriptor);
+
+        String expectedMessage = ERROR_MESSAGE_NO_SUCH_GROUP;
+
+        assertCommandFailure(command, model, expectedMessage);
     }
 
     @Test
     public void execute_invalidDirectory_throwCommandException() throws InvalidPathException {
-        Map<Id, Group> children = new HashMap<>();
-        Root root = new Root(children);
-        AbsolutePath currPath = new AbsolutePath("~/");
-        AbsolutePath path = new AbsolutePath("~/");
-        Model model = new ModelManager(currPath, root, new UserPrefs());
-
         EditStudentDescriptor editStudentDescriptor = new EditStudentDescriptor();
-        EditCommand editCommand = new EditCommand(path, editStudentDescriptor);
+        EditCommand command = new EditCommand(rootPath, editStudentDescriptor);
 
-        assertThrows(CommandException.class,
-                EditCommand.MESSAGE_INCORRECT_DIRECTORY_ERROR, () -> editCommand.execute(model));
+        String expectedMessage = MESSAGE_INCORRECT_DIRECTORY_ERROR;
+
+        assertCommandFailure(command, model, expectedMessage);
+    }
+
+    @Test
+    public void equals() throws InvalidPathException {
+        EditGroupDescriptor editGroupDescriptor1 = new EditGroupDescriptor();
+        EditGroupDescriptor editGroupDescriptor2 = new EditGroupDescriptor();
+        editGroupDescriptor1.setName(new Name("Group 123"));
+        editGroupDescriptor2.setName(new Name("Group 321"));
+        EditCommand command1 = new EditCommand(rootPath, editGroupDescriptor1);
+        EditCommand command2 = new EditCommand(rootPath, editGroupDescriptor2);
+
+        // same object -> returns true
+        assertEquals(command1, command1);
+
+        // same values -> returns true
+        EditCommand command1Copy = new EditCommand(rootPath, editGroupDescriptor1);
+        assertEquals(command1, command1Copy);
+
+        // different types -> returns false
+        assertNotEquals(1, command1);
+
+        // null -> returns false
+        assertNotEquals(null, command1);
+
+        // different values
+        assertNotEquals(command1, command2);
+    }
+
+    @Test
+    public void toStringMethod() {
+        EditGroupDescriptor editGroupDescriptor1 = new EditGroupDescriptor();
+        editGroupDescriptor1.setName(new Name("Group 123"));
+        EditCommand command = new EditCommand(rootPath, editGroupDescriptor1);
+        String expected = EditCommand.class.getCanonicalName()
+            + "{toEdit=" + editGroupDescriptor1 + "}";
+        assertEquals(expected, command.toString());
     }
 }
--- src/test/java/seedu/address/logic/commands/UnmarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnmarkCommandTest.java
@@ -4,72 +4,74 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.UnmarkCommand.MESSAGE_INVALID_INDEX;
+import static seedu.address.logic.commands.UnmarkCommand.MESSAGE_MARK_TASK_SUCCESS;
+import static seedu.address.testutil.TypicalGroups.GROUP_ONE;
 import static seedu.address.testutil.TypicalIndexes.FIRST_INDEX;
 import static seedu.address.testutil.TypicalIndexes.SECOND_INDEX;
+import static seedu.address.testutil.TypicalTasks.TASK_LIST_1;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.TaskOperation;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.path.AbsolutePath;
+import seedu.address.model.path.RelativePath;
 import seedu.address.model.path.exceptions.InvalidPathException;
+import seedu.address.model.profbook.Group;
 import seedu.address.model.profbook.Root;
 import seedu.address.model.task.Task;
-import seedu.address.model.util.SampleProfBook;
+import seedu.address.testutil.GroupBuilder;
+import seedu.address.testutil.RootBuilder;
 
 public class UnmarkCommandTest {
-
     private Model actualModel;
-
     private Model expectedModel;
-
-    private TaskOperation taskOperation;
+    private AbsolutePath rootPath = CommandTestUtil.getValidRootAbsolutePath();
+    private AbsolutePath displayPath;
 
     @BeforeEach
     public void setup() throws InvalidPathException {
-        AbsolutePath currentPath = new AbsolutePath("~/");
-        Root root1 = SampleProfBook.getRoot();
-        Root root2 = SampleProfBook.getRoot();
-        actualModel = new ModelManager(currentPath, root1, new UserPrefs());
-
-        expectedModel = new ModelManager(currentPath, root2, new UserPrefs());
+        Group groupOneWithTasks = new GroupBuilder(GROUP_ONE).withTaskList(TASK_LIST_1).build();
+        Root root = new RootBuilder().withGroup(groupOneWithTasks).build();
+        actualModel = new ModelManager(rootPath, new Root(root), new UserPrefs());
+        expectedModel = new ModelManager(rootPath, new Root(root), new UserPrefs());
 
         // Display task
-        AbsolutePath displayPath = new AbsolutePath("~/grp-002/");
+        RelativePath groupPath = new RelativePath(groupOneWithTasks.getId().toString());
+        displayPath = rootPath.resolve(groupPath);
+
         actualModel.setDisplayPath(displayPath);
         actualModel.showTaskList();
         expectedModel.setDisplayPath(displayPath);
         expectedModel.showTaskList();
-
-        taskOperation = expectedModel.taskOperation(displayPath);
     }
 
     @Test
     public void execute_validIndex_success() {
-        Task taskToUnmark = taskOperation.unmarkTask(FIRST_INDEX.getOneBased());
-        UnmarkCommand unmarkCommand = new UnmarkCommand(FIRST_INDEX);
+        TaskOperation operation = expectedModel.taskOperation(displayPath);
 
-        String expectedMessage = String.format(UnmarkCommand.MESSAGE_MARK_TASK_SUCCESS,
-                taskToUnmark.toString());
-
-        taskOperation.unmarkTask(FIRST_INDEX.getOneBased());
+        Task unmarkedTask = operation.unmarkTask(FIRST_INDEX.getOneBased());
         expectedModel.updateList();
 
-        assertCommandSuccess(unmarkCommand, actualModel, expectedMessage, expectedModel);
+        UnmarkCommand command = new UnmarkCommand(FIRST_INDEX);
+        String expectedMessage = String.format(MESSAGE_MARK_TASK_SUCCESS,
+                unmarkedTask.toString());
+
+        assertCommandSuccess(command, actualModel, expectedMessage, expectedModel);
     }
 
     @Test
     public void execute_invalidIndex_throwsCommandException() {
-        Index outOfBoundIndex = Index.fromOneBased(taskOperation.getTaskListSize() + 1);
-        UnmarkCommand unmarkCommand = new UnmarkCommand(outOfBoundIndex);
+        TaskOperation operation = actualModel.taskOperation(displayPath);
+        Index outOfBoundIndex = Index.fromOneBased(operation.getTaskListSize() + 1);
+        UnmarkCommand deleteCommand = new UnmarkCommand(outOfBoundIndex);
 
-        assertCommandFailure(unmarkCommand, actualModel,
-                Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, expectedModel);
+        assertCommandFailure(deleteCommand, actualModel, MESSAGE_INVALID_INDEX);
     }
 
     @Test
