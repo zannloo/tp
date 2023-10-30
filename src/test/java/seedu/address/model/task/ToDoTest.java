3f030e108f8f33296487fc54f6df62ed60270fff
Nid21cs <nid.nk45@gmail.com>
2023-10-31 00:40:25 +0800

    Create ToDoTest.java

---
+++ src/test/java/seedu/address/model/task/ToDoTest.java
@@ -0,0 +1,42 @@
+package seedu.address.model.task;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalTasks.TODO_1;
+import static seedu.address.testutil.TypicalTasks.TODO_2;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class ToDoTest {
+    private ToDo todo1 = TODO_1;
+    private ToDo toDo2 = TODO_2;
+    @Test
+    public void equals_nullObject_returnsFalse() {
+        assertFalse(todo1.equals(null));
+    }
+    @Test
+    public void equals_objectsOfDifferentClasses_returnsFalse() {
+        Object otherObject = new Object();
+        assertFalse(toDo2.equals(otherObject));
+    }
+
+    @Test
+    public void equals_differentToDos_returnsFalse() {
+        assertFalse(toDo2.equals(todo1));
+    }
+
+    @Test
+    public void markTask_taskIsMarked_returnsMarkedTask() {
+        ToDo markedTask = todo1.mark();
+        assertTrue(markedTask.getStatus());
+    }
+
+    @Test
+    public void unmarkTask_taskIsUnmarked_returnsUnmarkedTask() {
+        ToDo unmarkedTask = toDo2.unmark();
+        assertFalse(unmarkedTask.getStatus());
+    }
+
+}
