ace9570441bbed572761fe892069677ee88142e3
Nidhish Krishnan <nid.nk45@gmail.com>
2023-10-30 18:30:01 +0800

    Update CommandResultTest.java

--- src/test/java/seedu/address/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/address/logic/commands/CommandResultTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,9 +10,14 @@ import org.junit.jupiter.api.Test;
 public class CommandResultTest {
     private CommandResult commonCommandResult;
 
+    private static String FEEDBACK = "Test";
+
+    private static Boolean SHOW_HELP = true;
+    private static Boolean EXIT = false;
+
     @BeforeEach
     public void setUp() {
-        commonCommandResult = createCommandResult("Test", true, false);
+        commonCommandResult = createCommandResult(FEEDBACK, SHOW_HELP, EXIT);
     }
 
     @Test
@@ -46,13 +50,26 @@ public class CommandResultTest {
 
     @Test
     public void toString_correctOutput() {
-        String expectedOutput = "CommandResult{feedbackToUser='Test', showHelp=true, exit=false}";
-
+        String expectedOutput = CommandResult.class.getCanonicalName() + "{feedbackToUser=Test, showHelp=true, exit=false}";
         assertEquals(expectedOutput, commonCommandResult.toString());
     }
 
     private CommandResult createCommandResult(String feedbackToUser, boolean showHelp, boolean exit) {
         return new CommandResult(feedbackToUser, showHelp, exit);
     }
-}
+
+    @Test
+    public void getFeedback_correctOutput() {
+        assertEquals(commonCommandResult.getFeedbackToUser(), FEEDBACK);
+    }
+
+    @Test
+    public void showHelp_correctOutput() {
+        assertTrue(commonCommandResult.isShowHelp());
+    }
+
+    @Test
+    public void isExit_correctOutput() {
+        assertFalse(commonCommandResult.isExit());
+    }
 }
