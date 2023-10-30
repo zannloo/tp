d2807393f8d55734f9dd473ede175b8be9862b62
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-10-30 21:22:23 +0800

    Make default path as current path

--- src/main/java/seedu/address/logic/parser/CreateDeadlineCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CreateDeadlineCommandParser.java
@@ -12,7 +12,6 @@ import seedu.address.logic.commands.CreateDeadlineCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.path.AbsolutePath;
 import seedu.address.model.path.RelativePath;
-import seedu.address.model.path.exceptions.InvalidPathException;
 import seedu.address.model.task.Deadline;
 
 /**
@@ -32,28 +31,29 @@ public class CreateDeadlineCommandParser implements Parser<CreateDeadlineCommand
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, OPTION_DESC, OPTION_DATETIME, OPTION_ALL);
 
-        if (!ParserUtil.areOptionsPresent(argMultimap, OPTION_DESC, OPTION_DATETIME)
-                || argMultimap.getPreamble().isEmpty()) {
+        if (!ParserUtil.areOptionsPresent(argMultimap, OPTION_DESC, OPTION_DATETIME)) {
             throw new ParseException(String.format(
                     MESSAGE_INVALID_COMMAND_FORMAT, CreateDeadlineCommand.MESSAGE_USAGE));
         }
 
         argMultimap.verifyNoDuplicateOptionsFor(OPTION_DESC, OPTION_DATETIME, OPTION_ALL);
 
-        RelativePath path = ParserUtil.parseRelativePath(argMultimap.getPreamble());
-        AbsolutePath targetPath = null;
-        try {
-            targetPath = currPath.resolve(path);
-        } catch (InvalidPathException e) {
-            throw new ParseException(e.getMessage());
+        // If no path given, default to current path.
+        AbsolutePath fullTargetPath = null;
+        if (argMultimap.getPreamble().isEmpty()) {
+            fullTargetPath = currPath;
+        } else {
+            RelativePath target = ParserUtil.parseRelativePath(argMultimap.getPreamble());
+            fullTargetPath = ParserUtil.resolvePath(currPath, target);
         }
+
         LocalDateTime by = ParserUtil.parseDateTime(argMultimap.getValue(OPTION_DATETIME).get());
         Deadline deadline = new Deadline(argMultimap.getValue(OPTION_DESC).get(), by);
 
         if (argMultimap.getValue(OPTION_ALL).isEmpty()) {
-            return new CreateDeadlineCommand(targetPath, deadline, Category.NONE);
+            return new CreateDeadlineCommand(fullTargetPath, deadline, Category.NONE);
         }
         Category category = ParserUtil.parseCategory(argMultimap.getValue(OPTION_ALL).get());
-        return new CreateDeadlineCommand(targetPath, deadline, category);
+        return new CreateDeadlineCommand(fullTargetPath, deadline, category);
     }
 }
