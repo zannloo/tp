c3776050abcda320e0790c2882e87a01b4a664c5
Nid21cs <nid.nk45@gmail.com>
2023-10-31 02:13:31 +0800

    Update UserPrefsTest.java

--- src/test/java/seedu/address/model/UserPrefsTest.java
+++ src/test/java/seedu/address/model/UserPrefsTest.java
@@ -1,21 +1,41 @@
 package seedu.address.model;
 
 import static seedu.address.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserPrefsTest {
+        UserPrefs userPrefs;
+    @BeforeEach
+    public void setup() {
+        UserPrefs userPref = new UserPrefs();
+    }
 
     @Test
     public void setGuiSettings_nullGuiSettings_throwsNullPointerException() {
-        UserPrefs userPref = new UserPrefs();
-        assertThrows(NullPointerException.class, () -> userPref.setGuiSettings(null));
+        assertThrows(NullPointerException.class, () -> userPrefs.setGuiSettings(null));
     }
 
     @Test
     public void setAddressBookFilePath_nullPath_throwsNullPointerException() {
-        UserPrefs userPrefs = new UserPrefs();
         assertThrows(NullPointerException.class, () -> userPrefs.setAddressBookFilePath(null));
+    }
+
+    @Test
+    public void equalsNull_returnsFalse() {
+        assertFalse(userPrefs.equals(null));
+    }
+
+    @Test
+    public void hashCode_equalUserPrefs_returnsSameHashCode() {
+        UserPrefs test = new UserPrefs();
+        assertEquals(userPrefs.hashCode(), test.hashCode());
     }
 
 }
