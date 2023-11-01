35a89d86182c334f633eb195b2de63a85782c82d
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-11-02 02:07:36 +0800

    Refactor with new ReadOnlyTaskList

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -30,6 +30,7 @@ import seedu.address.model.profbook.Name;
 import seedu.address.model.profbook.Phone;
 import seedu.address.model.profbook.Student;
 import seedu.address.model.task.ReadOnlyTaskList;
+import seedu.address.model.task.TaskListManager;
 
 /**
  * EditCommand is a class representing a command to edit the details of a person (either a student or a group) in
@@ -238,7 +239,7 @@ public class EditCommand extends Command {
         Email updatedEmail = editStudentDescriptor.getEmail().orElse(studentToEdit.getEmail());
         Address updatedAddress = editStudentDescriptor.getAddress().orElse(studentToEdit.getAddress());
         StudentId updatedId = editStudentDescriptor.getId().orElse(studentToEdit.getId());
-        ReadOnlyTaskList taskList = new ReadOnlyTaskList(studentToEdit.getAllTasks());
+        ReadOnlyTaskList taskList = new TaskListManager(studentToEdit.getAllTasks());
         return new Student(taskList, updatedName, updatedEmail, updatedPhone, updatedAddress, updatedId);
     }
 
@@ -250,7 +251,7 @@ public class EditCommand extends Command {
         assert groupToEdit != null;
         Name updatedName = editGroupDescriptor.getName().orElse(groupToEdit.getName());
         GroupId updatedId = editGroupDescriptor.getId().orElse(groupToEdit.getId());
-        ReadOnlyTaskList taskList = new ReadOnlyTaskList(groupToEdit.getAllTask());
+        ReadOnlyTaskList taskList = new TaskListManager(groupToEdit.getAllTasks());
         Map<Id, Student> students = groupToEdit.getChildren();
         return new Group(taskList, students, updatedName, updatedId);
     }
