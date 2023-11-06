7240c09e25f389c946d7a2f21b615a4ad2d322e7
NgChunMan <chunman03@gmail.com>
2023-11-07 00:47:56 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
@@ -3,14 +3,14 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_MISSING_ARGUMENT;
 import static seedu.address.logic.commands.CommandTestUtil.EMPTY_PREAMBLE;
 import static seedu.address.logic.commands.CommandTestUtil.HELP_OPTION;
-import static seedu.address.logic.commands.DeleteTaskCommand.COMMAND_WORD;
+import static seedu.address.logic.commands.EditCommand.COMMAND_WORD;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.model.path.AbsolutePath.ROOT_PATH;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.DeleteTaskCommand;
+import seedu.address.logic.commands.EditCommand;
 
 public class EditCommandParserTest {
 
@@ -19,13 +19,13 @@ public class EditCommandParserTest {
     @Test
     public void parse_help_returnsHelpMessage() {
         // help option only
-        assertParseSuccess(parser, HELP_OPTION, ROOT_PATH, DeleteTaskCommand.HELP_MESSAGE);
+        assertParseSuccess(parser, HELP_OPTION, ROOT_PATH, EditCommand.HELP_MESSAGE);
 
         // valid path with help option
-        assertParseSuccess(parser, "grp-001" + HELP_OPTION, ROOT_PATH, DeleteTaskCommand.HELP_MESSAGE);
+        assertParseSuccess(parser, "grp-001" + HELP_OPTION, ROOT_PATH, EditCommand.HELP_MESSAGE);
 
         // invalid path with help option
-        assertParseSuccess(parser, "0001Y" + HELP_OPTION, ROOT_PATH, DeleteTaskCommand.HELP_MESSAGE);
+        assertParseSuccess(parser, "0001Y" + HELP_OPTION, ROOT_PATH, EditCommand.HELP_MESSAGE);
     }
 
     @Test
