14dbcecb5b824b16ffd1d73eca53cb8d791f4a7d
Nereus Ng Wei Bin <iamnereusng@gmail.com>
2023-11-08 13:10:29 +0800

    Edit comments

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -22,14 +22,11 @@ import seedu.address.ui.Displayable;
  */
 public class LogicManager implements Logic {
     public static final String FILE_OPS_ERROR_FORMAT = "Could not save data due to the following error: %s";
-
     public static final String FILE_OPS_PERMISSION_ERROR_FORMAT =
             "Could not save data to file %s due to insufficient permissions to write to the file or the folder.";
 
     private final Logger logger = LogsCenter.getLogger(LogicManager.class);
-
     private final Model model;
-
     private final ProfBookStorage storage;
     private final ProfBookParser profBookParser;
 
